82c8e3f77a781c74d5b7b09d19174f2dfa156b65
Patchett <rybridges16@gmail.com>
2016-02-27 17:51:03 -0800

    Updated GridStatusKalecgos to use the slider for curse duration

--- Grid Addons/GridStatusKalecgos/GridStatusKalecgos.lua
+++ Grid Addons/GridStatusKalecgos/GridStatusKalecgos.lua
@@ -5,50 +5,50 @@ local L = AceLibrary("AceLocale-2.2"):new("GridStatusKalecgos")
 L:RegisterTranslations("enUS", function()
         return {
             ["Kalecgos Curse"] = true,
-            ["Color < 13 sec"] = true,
-            ["Color when someone has had the curse for less than 13 seconds"] = true,
-            ["Color >= 13 sec"] = true,
-            ["Color when someone has had the curse for 13 or more seconds"] = true,
+            ["Color < 10 sec"] = true,
+            ["Color when someone has had the curse for less than 10 seconds"] = true,
+            ["Color >= 10 sec"] = true,
+            ["Color when someone has had the curse for 10 or more seconds"] = true,
         }
     end)
 
 L:RegisterTranslations("deDE", function()
         return {
              ["Kalecgos Curse"] = "Kalecgos Fluch" ,
-             ["Color < 13 sec"] = "Farbe <13 Sekunden",
-             ["Color when someone has had the curse for less than 13 seconds"] ="Farbe  falls jemand den Debuff weniger als 13 Sekunden hat",
-             ["Color >= 13 sec"] = "Farbe >= 13 Sekunden",
-             ["Color when someone has had the curse for 13 or more seconds"] = "Farbe  falls jemand den Debuff länger als 13 Sekunden hat",
+             ["Color < 10 sec"] = "Farbe <10 Sekunden",
+             ["Color when someone has had the curse for less than 10 seconds"] ="Farbe falls jemand den Debuff weniger als 10 Sekunden hat",
+             ["Color >= 10 sec"] = "Farbe >= 10 Sekunden",
+             ["Color when someone has had the curse for 10 or more seconds"] = "Farbe falls jemand den Debuff länger als 10 Sekunden hat",
         }
     end)
 
 L:RegisterTranslations("koKR", function()
         return {
             ["Kalecgos Curse"] = "칼렉고스 저주",
-            ["Color < 13 sec"] = "색상 < 13 초",
-            ["Color when someone has had the curse for less than 13 seconds"] = "13초 미만의 저주 색상을 설정합니다.",
-            ["Color >= 13 sec"] = "색상 >= 13 초",
-            ["Color when someone has had the curse for 13 or more seconds"] = "13초 이상의 저주 색상을 설정합니다.",
+            ["Color < 10 sec"] = "색상 < 10 초",
+            ["Color when someone has had the curse for less than 10 seconds"] = "10초 미만의 저주 색상을 설정합니다.",
+            ["Color >= 10 sec"] = "색상 >= 10 초",
+            ["Color when someone has had the curse for 10 or more seconds"] = "10초 이상의 저주 색상을 설정합니다.",
         }
     end)
 
 L:RegisterTranslations("zhCN", function()
         return {
             ["Kalecgos Curse"] = "卡雷苟斯诅咒",
-            ["Color < 13 sec"] = "高亮小于13秒",
-            ["Color when someone has had the curse for less than 13 seconds"] = "高亮当有人诅咒时间小于13秒。",
-            ["Color >= 13 sec"] = "高亮大于等于13秒",
-            ["Color when someone has had the curse for 13 or more seconds"] = "高亮当有人诅咒时间大于等于13秒。",
+            ["Color < 10 sec"] = "高亮小于10秒",
+            ["Color when someone has had the curse for less than 10 seconds"] = "高亮当有人诅咒时间小于10秒。",
+            ["Color >= 10 sec"] = "高亮大于等于10秒",
+            ["Color when someone has had the curse for 10 or more seconds"] = "高亮当有人诅咒时间大于等于10秒。",
         }
     end)
 
 L:RegisterTranslations("zhTW", function()
         return {
             ["Kalecgos Curse"] = "卡雷苟斯詛咒",
-            ["Color < 13 sec"] = "高亮小於13秒",
-            ["Color when someone has had the curse for less than 13 seconds"] = "高亮當有人詛咒時間小於13秒。",
-            ["Color >= 13 sec"] = "高亮大於等於13秒",
-            ["Color when someone has had the curse for 13 or more seconds"] = "高亮當有人詛咒時間大於等於13秒。",
+            ["Color < 10 sec"] = "高亮小於10秒",
+            ["Color when someone has had the curse for less than 10 seconds"] = "高亮當有人詛咒時間小於10秒。",
+            ["Color >= 10 sec"] = "高亮大於等於10秒",
+            ["Color when someone has had the curse for 10 or more seconds"] = "高亮當有人詛咒時間大於等於10秒。",
         }
     end)
 
@@ -98,6 +98,7 @@ GridStatusKalecgos.defaultDB = {
             b = 0,
             a = 1,
         },
+        ["timer"] = 10,
     },
 }
 
@@ -105,39 +106,54 @@ local curseOptions = {
     ["color"] = false,
     ["color1"] = {
         type = "color",
-        name = L["Color < 13 sec"],
-        desc = L["Color when someone has had the curse for less than 13 seconds"],
+        name = L["Color < 10 sec"],
+        desc = L["Color when someone has had the curse for less than 10 seconds"],
         order = 88,
         hasAlpha = true,
         get = function ()
-                  local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color1
-                  return color.r, color.g, color.b, color.a
-              end,
+            local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color1
+            return color.r, color.g, color.b, color.a
+        end,
         set = function (r, g, b, a)
-                  local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color1
-                  color.r = r
-                  color.g = g
-                  color.b = b
-                  color.a = a or 1
-              end,
+            local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color1
+            color.r = r
+            color.g = g
+            color.b = b
+            color.a = a or 1
+        end,
     },
     ["color2"] = {
         type = "color",
-        name = L["Color >= 13 sec"],
-        desc = L["Color when someone has had the curse for 13 or more seconds"],
+        name = L["Color >= 10 sec"],
+        desc = L["Color when someone has had the curse for 10 or more seconds"],
         order = 89,
         hasAlpha = true,
         get = function ()
-                  local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color2
-                  return color.r, color.g, color.b, color.a
-              end,
+            local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color2
+            return color.r, color.g, color.b, color.a
+        end,
         set = function (r, g, b, a)
-                  local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color2
-                  color.r = r
-                  color.g = g
-                  color.b = b
-                  color.a = a or 1
-              end,
+            local color = GridStatusKalecgos.db.profile["alert_kalecgos"].color2
+            color.r = r
+            color.g = g
+            color.b = b
+            color.a = a or 1
+        end,
+    },
+    ["timer"] = {
+        type = "range",
+        name = "Threshold",
+        desc = "Time in seconds when the color change occurs (default 10)",
+        order = 89.5,
+        min = 0,
+        max = 20,
+        step = 1,
+        get = function()
+            return GridStatusKalecgos.db.profile.alert_kalecgos.timer
+        end,
+        set = function(v)
+            GridStatusKalecgos.db.profile.alert_kalecgos.timer = v
+        end,
     },
 }
 
@@ -185,12 +201,12 @@ function GridStatusKalecgos:SpecialEvents_UnitDebuffGained(unit, debuff, apps, t
     end
 
     if debuff == BS["Curse of Boundless Agony"] then
-    local settings = self.db.profile["alert_kalecgos"]
+        local settings = self.db.profile["alert_kalecgos"]
 
         self:Debug("Curse Gained:", debuff)
         self:Debug("Curse Gained:", BS["Curse of Boundless Agony"])
 
-    -- Immediately start at color1 to overwrite any lower priority icons before the next timer tick
+        -- Immediately start at color1 to overwrite any lower priority icons before the next timer tick
         self.core:SendStatusGained(UnitName(unit), "alert_kalecgos",
                                    settings.priority, false,
                                    settings.color1,
@@ -239,8 +255,8 @@ function GridStatusKalecgos:DebuffTimer()
 
         local color = settings.color1
 
-        -- use color2 if unit has had the debuff for 13 or more seconds
-        if elapsed >= 13 then
+        -- use color2 if unit has had the debuff for 10 or more seconds
+        if elapsed >= (settings.timer or 10) then
             color = settings.color2
         end
 
